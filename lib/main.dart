73c369277f57fd9760f0d0bde1e31eb4695eeb03
veerapatadia <patadiaveera@gmail.com>
2024-08-05 22:19:01 +0530

    05/08/24

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:animator_app/views/HomePage.dart';
+import 'package:animator_app/views/splashscreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        // '/': (context) => Splashscreen(),
+        'home_page': (context) => HomePage(),
+      },
+    );
+  }
+}
